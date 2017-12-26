3de2425b3faa8cb1d7124e6aa6ac78096c9042df
codefir <pricop2008@yahoo.com>
2017-12-26 02:57:32 +0200

    Add files via upload

---
+++ languages/deutsch.php
@@ -0,0 +1,232 @@
+<?php
+// Language Name
+$name = 'Deutsch';
+
+// Language Author
+$author = 'MeinWetter';
+
+// Language URL
+$url = 'http://meinwetter.info';
+
+// Language Direction
+$direction = 'ltr';
+
+$lang['info'] = 'Info';
+$lang['themes'] = 'Themen';
+$lang['language'] = 'Sprache';
+$lang['languages'] = 'Sprachen';
+
+// Admin Panel
+$lang['admin'] = 'Admin';
+$lang['general'] = 'Allgemein';
+$lang['password'] = 'Passwort';
+$lang['info_pages'] = 'Info Seiten';
+$lang['username'] = 'Benutzername';
+$lang['password'] = 'Passwort';
+$lang['login'] = 'Login';
+$lang['logout'] = 'Logout';
+$lang['save'] = 'Speichern';
+$lang['remember_me'] = 'Login merken';
+$lang['recover'] = 'Wiederherstellen';
+$lang['by'] = 'Von';
+$lang['active'] = 'Aktiv';
+$lang['activate'] = 'Aktivieren';
+$lang['default'] = 'Standard';
+$lang['make_default'] = 'Als Standard einstellen';
+$lang['public'] = 'Öffentlich';
+$lang['unlisted'] = 'Ungelistet';
+$lang['edit'] = 'Bearbeiten';
+$lang['new_page'] = 'Neue Seite';
+$lang['delete'] = 'Löschen';
+$lang['cancel'] = 'Abbrechen';
+$lang['yes'] = 'Ja';
+$lang['no'] = 'Nein';
+$lang['off'] = 'Aus';
+$lang['on'] = 'An';
+
+$lang['dashboard'] = 'Dashboard';
+$lang['site_title'] = 'Seiten Titel';
+$lang['site_logo'] = 'Seiten Logo';
+$lang['tracking_code'] = 'Tracking Code';
+$lang['current_password'] = 'Aktuelles Passwort';
+$lang['new_password'] = 'Neues Passwort';
+$lang['confirm_new_password'] = 'Neues Passwort bestätigen';
+
+$lang['settings_saved'] = 'Einstellungen gespeichert';
+
+$lang['upload_error_code'] = 'Upload Fehler Code: %s';
+$lang['invalid_user_pass'] = 'Ungültiger Benutzername/Passwort';
+$lang['wrong_current_password'] = 'Das eingegebene Passwort ist falsch';
+$lang['password_too_short'] = 'Das eingegebene Passwort ist zu kurz';
+$lang['password_not_matching'] = 'Die eingegebenen Passwörter stimmen nicht überein';
+$lang['all_fields_required'] = 'Alle Felder sind Pflichtfelder';
+
+$lang['page_title'] = 'Seiten Titel';
+$lang['page_url'] = 'Seiten URL';
+$lang['page_public'] = 'Öffentliche Seite';
+$lang['page_content'] = 'Seiten Inhalt';
+$lang['page_url_exists'] = 'Die Seiten URL existiert bereits';
+$lang['page_created'] = 'Seite %s wurde erstellt';
+$lang['page_deleted'] = 'Seite %s wurde gelöscht';
+
+$lang['site_info'] = 'Seiten Info';
+$lang['useful_links'] = 'Nützliche Links';
+$lang['get_more_themes'] = 'Erhalte mehr Themen';
+$lang['get_more_languages'] = 'Erhalte mehr Sprachen';
+
+$lang['ads'] = 'Werbung';
+$lang['ads_1'] = 'Home Page (Oben)';
+$lang['ads_2'] = 'Home Page (Mitte)';
+$lang['ads_3'] = 'Home Page (Unten)';
+$lang['ad_unit_code'] = 'Code der Werbung';
+
+$lang['weather_api_key'] = 'API Schlüssel';
+$lang['weather_location'] = 'Standard Region';
+$lang['weather_format'] = 'Standard Format';
+$lang['weather_geo_location'] = 'Geolocation';
+$lang['weather_forecast_days'] = 'Wetterprognose in Tagen (0-5)';
+$lang['weather_latest'] = 'Letzte Suchen';
+
+// Home
+$lang['preferences'] = 'Einstellungen';
+
+// Preferences
+$lang['theme'] = 'Thema';
+$lang['dark_mode'] = 'Aktiviere dunkles Thema';
+$lang['site_language'] = 'Sprache';
+
+// Misc
+$lang['token_mismatch'] = 'Token fehlerhaft';
+$lang['copyright'] = 'Copyright &copy; %s %s.';
+$lang['all_rights_reserved'] = 'Alle Rechte vorbehalten';
+$lang['powered_by'] = 'Powered by %s.';
+
+// Error
+$lang['no_results_found'] = 'Keine Ergebnisse gefunden für %s.';
+$lang['suggestions'] = 'Vorschläge:';
+$lang['suggestion_1'] = 'Versuche den Ort mit dem Ländercode zu suchen (z.B. DE, UK, FR usw.)';
+$lang['suggestion_2'] = 'Versuche einen Ort in deiner Nähe zu finden falls sich deiner nicht in der Datenbank befindet';
+$lang['suggestion_3'] = 'Solltest du immernoch keine Ergebnisse erhalten wende dich bitte an uns: support@meinwetter.info';
+
+// Weather
+$lang['weather'] = 'Wetter';
+$lang['forecast'] = 'Prognose';
+$lang['evolution'] = 'Entwicklung';
+$lang['conditions'] = 'Wetter Bedingungen';
+$lang['lowest_temperature'] = 'Kälteste Temperatur';
+$lang['highest_temperature'] = 'Höchste Temperatur';
+$lang['temperature'] = 'Temperatur';
+$lang['humidity'] = 'Luftfeuchtigkeit';
+$lang['wind_speed'] = 'Windgeschwindigkeit';
+$lang['wind_direction'] = 'Windrichtung';
+$lang['city_name'] = 'Stadtname';
+$lang['latest_searches'] = 'Letzte Suchen';
+$lang['date_format'] = '%1$s/%2$s/%3$s';
+$lang['hour_format'] = '%1$s:%2$s';
+$lang['kmph'] = 'km/h';
+$lang['mph'] = 'mph';
+$lang['monday'] = 'Montag';
+$lang['tuesday'] = 'Dienstag';
+$lang['wednesday'] = 'Mittwoch';
+$lang['thursday'] = 'Donnerstag';
+$lang['friday'] = 'Freitag';
+$lang['saturday'] = 'Samstag';
+$lang['sunday'] = 'Sonntag';
+$lang['favorites'] = 'Favoriten';
+$lang['search_results'] = 'Suchergebnisse';
+$lang['daily_forecast_evolution'] = 'Tägliche Vorhersagenentwicklung';
+$lang['hourly_forecast_evolution'] = 'Stündliche Vorhersagenentwicklung';
+$lang['daily_forecast'] = 'Tägliche Prognose';
+$lang['hourly_forecast'] = 'Stündliche Prognose';
+$lang['sunrise'] = 'Sonnenaufgang';
+$lang['sunset'] = 'Sonnenuntergang';
+$lang['latitude'] = 'Breitengrad';
+$lang['longitude'] = 'Längengrad';
+$lang['timezone'] = 'Zeitzone: GMT%s';
+
+$lang['f'] = 'F';
+$lang['c'] = 'C';
+$lang['fahrenheit'] = 'Fahrenheit';
+$lang['celsius'] = 'Celsius';
+
+$lang['c_200'] = 'Gewitter mit leichten Regen';
+$lang['c_201'] = 'Gewitter mit Regen';
+$lang['c_202'] = 'Gewitter mit starken Regen';
+$lang['c_210'] = 'leichte Gewitter';
+$lang['c_211'] = 'Gewitter';
+$lang['c_212'] = 'Unwetter';
+$lang['c_221'] = 'zackiges Gewitter';
+$lang['c_230'] = 'Gewitter mit Nieselregen';
+$lang['c_231'] = 'Gewitter mit Nieselregen';
+$lang['c_232'] = 'Gewitter mit starken Nieselregen';
+
+$lang['c_300'] = 'leichter Nebel';
+$lang['c_301'] = 'Nebel';
+$lang['c_302'] = 'starker Nebel';
+$lang['c_310'] = 'ganz leichter Nebel';
+$lang['c_311'] = 'Nieselregen';
+$lang['c_312'] = 'stärkerer Nieselregen';
+$lang['c_313'] = 'Platzregen mit Nebel';
+$lang['c_314'] = 'starker Platzregen mit Nebel';
+$lang['c_321'] = 'Platzregen Nebel';
+
+$lang['c_500'] = 'leichter Regen';
+$lang['c_501'] = 'moderater Regen';
+$lang['c_502'] = 'starker Regen';
+$lang['c_503'] = 'sehr starker Regen';
+$lang['c_504'] = 'Platzregen';
+$lang['c_511'] = 'Hagel';
+$lang['c_520'] = 'Platzregen';
+$lang['c_521'] = 'starker Platzregen';
+$lang['c_522'] = 'sehr starker Platzregen';
+$lang['c_531'] = 'zackiger Platzregen';
+
+$lang['c_600'] = 'leichter Schneefall';
+$lang['c_601'] = 'Schneefall';
+$lang['c_602'] = 'starker Schneefall';
+$lang['c_611'] = 'Schneeregen';
+$lang['c_612'] = 'starker Schneeregen';
+$lang['c_615'] = 'leichter Regen mit Schnee';
+$lang['c_616'] = 'Regen und Schnee';
+$lang['c_620'] = 'leichter Schneeregen';
+$lang['c_621'] = 'Schneeregen';
+$lang['c_622'] = 'starker Schneeregen';
+
+$lang['c_701'] = 'Nebel';
+$lang['c_711'] = 'Rauch';
+$lang['c_721'] = 'Dunst';
+$lang['c_731'] = 'Sand und Staubwirbel';
+$lang['c_741'] = 'Nebel';
+$lang['c_751'] = 'Samd';
+$lang['c_761'] = 'Staub';
+$lang['c_762'] = 'Vulcan Asche';
+$lang['c_771'] = 'Böen';
+$lang['c_781'] = 'Tornado';
+
+$lang['c_800'] = 'klarer Himmel';
+
+$lang['c_801'] = 'leicht bewölkt';
+$lang['c_802'] = 'verstreute Wolken';
+$lang['c_803'] = 'gebrochene Wolkendecke';
+$lang['c_804'] = 'bedeckte Wolkendecke';
+
+$lang['c_900'] = 'Tornado';
+$lang['c_901'] = 'Tropischer Sturm';
+$lang['c_902'] = 'Hurrikan';
+$lang['c_903'] = 'kalt';
+$lang['c_904'] = 'warm';
+$lang['c_905'] = 'windig';
+$lang['c_906'] = 'Hagel';
+
+$lang['c_951'] = 'ruhig';
+$lang['c_952'] = 'leichte Briese';
+$lang['c_953'] = 'leichte Briese';
+$lang['c_954'] = 'moderate Briese';
+$lang['c_955'] = 'frische Briese';
+$lang['c_956'] = 'starke Briese';
+$lang['c_957'] = 'starker Wind, fast Sturm';
+$lang['c_958'] = 'Sturm';
+$lang['c_959'] = 'schwerer Sturm';
+$lang['c_960'] = 'Sturm';
+$lang['c_961'] = 'heftiger Sturm';
+$lang['c_962'] = 'Hurrikan';
