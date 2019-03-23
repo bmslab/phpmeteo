015c831409f403909c557179e728e11da531ed82
codefir <pricop2008@yahoo.com>
2019-03-23 02:11:30 +0200

    Add files via upload

---
+++ languages/dutch.php
@@ -0,0 +1,235 @@
+<?php
+// Language Name
+$name = 'Dutch';
+
+// Language Author
+$author = 'WhooX';
+
+// Language URL
+$url = 'https://phpmeteo.com';
+
+// Language Direction
+$lang['lang_dir'] = 'ltr';
+
+$lang['info'] = 'Info';
+$lang['themes'] = 'Thema\'s';
+$lang['language'] = 'Taal';
+$lang['languages'] = 'Talen';
+
+// Admin Panel
+$lang['admin'] = 'Admin';
+$lang['general'] = 'Algemeen';
+$lang['info_pages'] = 'Info Pagina\'s';
+$lang['username'] = 'Gebruikersnaam';
+$lang['password'] = 'Paswoord';
+$lang['login'] = 'Inloggen';
+$lang['logout'] = 'Uitloggen';
+$lang['save'] = 'Opslaan';
+$lang['remember_me'] = 'Onthoud mij';
+$lang['recover'] = 'Herstel';
+$lang['by'] = 'Door';
+$lang['active'] = 'Actief';
+$lang['activate'] = 'Activeer';
+$lang['default'] = 'Standaard';
+$lang['make_default'] = 'Maak standaard';
+$lang['public'] = 'Publiek';
+$lang['unlisted'] = 'Privaat';
+$lang['edit'] = 'Editeer';
+$lang['new_page'] = 'Nieuwe pagina';
+$lang['delete'] = 'Verwijder';
+$lang['cancel'] = 'Annuleer';
+$lang['yes'] = 'Ja';
+$lang['no'] = 'Nee';
+$lang['off'] = 'Uit';
+$lang['on'] = 'Aan';
+
+$lang['dashboard'] = 'Dashboard';
+$lang['site_title'] = 'Site titel';
+$lang['site_logo'] = 'Site logo';
+$lang['site_favicon'] = 'Site favicon';
+$lang['timezone'] = 'Tijdzone';
+$lang['tracking_code'] = 'Tracking code';
+$lang['current_password'] = 'Huidig paswoord';
+$lang['new_password'] = 'Nieuw paswoord';
+$lang['confirm_new_password'] = 'Bevestig nieuw paspoort';
+
+$lang['settings_saved'] = 'Instellingen opgeslagen';
+
+$lang['upload_error_code'] = 'Upload foutcode: %s';
+$lang['invalid_user_pass'] = 'Ongeldige gebruikersnaam of paswoord';
+$lang['wrong_current_password'] = 'Je hebt een ongeldig paswoord ingevoerd';
+$lang['password_too_short'] = 'Paswoord is te kort';
+$lang['password_not_matching'] = 'Paswoord komt niet overeen';
+$lang['all_fields_required'] = 'Alle velden zijn noodzakelijk';
+
+$lang['page_title'] = 'Pagina titel';
+$lang['page_url'] = 'Pagina URL';
+$lang['page_public'] = 'Publieke pagina';
+$lang['page_content'] = 'Pagina inhoud';
+$lang['page_url_exists'] = 'De pagina URL bestaat reeds';
+$lang['page_created'] = 'Pagina %s werd aangemaakt';
+$lang['page_deleted'] = 'Pagina %s werd verwijdeerd';
+
+$lang['site_info'] = 'Site Info';
+$lang['useful_links'] = 'Nuttige koppelingen';
+$lang['get_more_themes'] = 'Meer thema\'s';
+$lang['get_more_languages'] = 'Meer talen';
+
+$lang['ads'] = 'Reclame';
+$lang['ads_1'] = 'Home pagina (top)';
+$lang['ads_2'] = 'Home pagina (midden)';
+$lang['ads_3'] = 'Home pagina (bodem)';
+$lang['ad_unit_code'] = 'Reclameblok code';
+
+$lang['weather_api_key'] = 'API sleutel';
+$lang['weather_location'] = 'Standaard Locatie';
+$lang['weather_format'] = 'Standaard formaat';
+$lang['weather_geo_location'] = 'Geolocatie';
+$lang['weather_forecast_days'] = 'Voorspelling in dagen (1-5)';
+$lang['weather_latest'] = 'Laatste zoekopdrachten';
+$lang['searches_per_ip'] = 'Zoekopdrachten / IP';
+
+// Home
+$lang['preferences'] = 'Voorkeuren';
+
+// Preferences
+$lang['theme'] = 'Thema';
+$lang['dark_mode'] = 'Donkere modus';
+$lang['site_language'] = 'Site taal';
+
+// Misc
+$lang['token_mismatch'] = 'Ongeldig token';
+$lang['copyright'] = '&copy; %s %s';
+$lang['powered_by'] = 'Powered by %s';
+$lang['ellipsis'] = '...';
+
+// Error
+$lang['search_l_e'] = 'Dagelijks zoekquota bereikt, probeer morgen opnieuw.';
+$lang['no_results_found'] = 'Geen resultaten voor %s.';
+$lang['suggestions'] = 'Suggesties:';
+$lang['suggestion_1'] = 'Probeer te zoeken naar een stad en de code van het land.';
+$lang['suggestion_2'] = 'Probeer te zoeken naar een stad en de naam van het land.';
+$lang['suggestion_3'] = 'Probeer te zoeken naar bijgelegen steden.';
+
+// Weather
+$lang['weather'] = 'Weer';
+$lang['forecast'] = 'Voorspelling';
+$lang['evolution'] = 'Evolutie';
+$lang['conditions'] = 'Condities';
+$lang['lowest_temperature'] = 'Laagste temperatuur';
+$lang['highest_temperature'] = 'Hoogste temperatuur';
+$lang['temperature'] = 'Temperatuur';
+$lang['humidity'] = 'Luchtvochtigheid';
+$lang['wind_speed'] = 'Windsnelheid';
+$lang['wind_direction'] = 'Windrichting';
+$lang['city_name'] = 'Naam stad';
+$lang['latest_searches'] = 'Recente zoekopdrachten';
+$lang['date_format'] = '%1$s/%2$s/%3$s';
+$lang['hour_format'] = '%1$s:%2$s';
+$lang['kmph'] = 'km/h';
+$lang['mph'] = 'mph';
+$lang['monday'] = 'Maandag';
+$lang['tuesday'] = 'Dinsdag';
+$lang['wednesday'] = 'Woensdag';
+$lang['thursday'] = 'Donderdag';
+$lang['friday'] = 'Vrijdag';
+$lang['saturday'] = 'Zaterdag';
+$lang['sunday'] = 'Zondag';
+$lang['favorites'] = 'Favorieten';
+$lang['search_results'] = 'Zoekresultaten';
+$lang['daily_forecast_evolution'] = 'Evolutie voorspelling per dag';
+$lang['hourly_forecast_evolution'] = 'Evolutie voorspelling per uur';
+$lang['daily_forecast'] = 'Voorspelling per dag';
+$lang['hourly_forecast'] = 'Voorspelling per uur';
+$lang['sunrise'] = 'Zonsopgang';
+$lang['sunset'] = 'Zonsondergang';
+$lang['latitude'] = 'Breedtegraad';
+$lang['longitude'] = 'Lengtegraad';
+$lang['timezone_gmt'] = 'Tijdzone: GMT%s';
+
+$lang['f'] = 'F';
+$lang['c'] = 'C';
+$lang['fahrenheit'] = 'Fahrenheit';
+$lang['celsius'] = 'Celsius';
+
+$lang['c_200'] = 'onweer met lichte regen';
+$lang['c_201'] = 'onweer met regen';
+$lang['c_202'] = 'onweer met hevige regen';
+$lang['c_210'] = 'light onweer';
+$lang['c_211'] = 'onweer';
+$lang['c_212'] = 'hevig onweer';
+$lang['c_221'] = 'verspreid onweer';
+$lang['c_230'] = 'onweer met lichte motregen';
+$lang['c_231'] = 'onweer met motregen';
+$lang['c_232'] = 'onweer met hevige motregen';
+
+$lang['c_300'] = 'lichte motegen';
+$lang['c_301'] = 'motregen';
+$lang['c_302'] = 'matige motregen';
+$lang['c_310'] = 'lichte motregen, regen';
+$lang['c_311'] = 'motregen, regen';
+$lang['c_312'] = 'hevige motregen, regen';
+$lang['c_313'] = 'buien van regen en motregen';
+$lang['c_314'] = 'hevige regenbuien en motregen';
+$lang['c_321'] = 'buien van motregen';
+
+$lang['c_500'] = 'lichte regen';
+$lang['c_501'] = 'matige regen';
+$lang['c_502'] = 'hevige regen';
+$lang['c_503'] = 'zeer hevige regen';
+$lang['c_504'] = 'extreme regen';
+$lang['c_511'] = 'ijsregen';
+$lang['c_520'] = 'lichte regenbuien';
+$lang['c_521'] = 'regenbuien';
+$lang['c_522'] = 'hevige regenbuien';
+$lang['c_531'] = 'verspreide regenbuien';
+
+$lang['c_600'] = 'lichte sneeuw';
+$lang['c_601'] = 'sneeuw';
+$lang['c_602'] = 'hevige sneeuw';
+$lang['c_611'] = 'korrelsneeuw';
+$lang['c_612'] = 'buien van korrelsneeuw';
+$lang['c_615'] = 'lichte smeltende sneeuw';
+$lang['c_616'] = 'smeltende sneeuw';
+$lang['c_620'] = 'lichte sneeuwbuien';
+$lang['c_621'] = 'sneeuwbuien';
+$lang['c_622'] = 'hevige sneeuwbuien';
+
+$lang['c_701'] = 'mist';
+$lang['c_711'] = 'rook';
+$lang['c_721'] = 'nevel';
+$lang['c_731'] = 'zand en stof wervelwind';
+$lang['c_741'] = 'mist';
+$lang['c_751'] = 'zand';
+$lang['c_761'] = 'stof';
+$lang['c_762'] = 'vulkanische as';
+$lang['c_771'] = 'rukwinden';
+$lang['c_781'] = 'tornado';
+
+$lang['c_800'] = 'helder';
+
+$lang['c_801'] = 'weinig wolken';
+$lang['c_802'] = 'verspreide wolkenvelden';
+$lang['c_803'] = 'opklaringen';
+$lang['c_804'] = 'volledig bewolkt';
+
+$lang['c_900'] = 'tornado';
+$lang['c_901'] = 'tropische storm';
+$lang['c_902'] = 'orkaan';
+$lang['c_903'] = 'koud';
+$lang['c_904'] = 'warm';
+$lang['c_905'] = 'winderig';
+$lang['c_906'] = 'hagel';
+
+$lang['c_951'] = 'kalm';
+$lang['c_952'] = 'lichte bries';
+$lang['c_953'] = 'kalme bries';
+$lang['c_954'] = 'matige bries';
+$lang['c_955'] = 'frisse bries';
+$lang['c_956'] = 'sterke bries';
+$lang['c_957'] = 'veel wind';
+$lang['c_958'] = 'wind';
+$lang['c_959'] = 'sterke wind';
+$lang['c_960'] = 'storm';
+$lang['c_961'] = 'sterke storm';
+$lang['c_962'] = 'orkaan';
